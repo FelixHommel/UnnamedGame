e91fece97fefd5601dec39406dbbf426b857d9bb
Felix Hommel <felixhommel04@gmail.com>
2025-10-10 19:33:20 +0200

    feat(exceptions): add exception that repositories can throw when something goes wrong

---
+++ src/core/common/exceptions/RepositoryException.hpp
@@ -0,0 +1,32 @@
+#ifndef SRC_CORE_COMMON_EXCEPTIONS_REPOSITORY_EXCEPTION_HPP
+#define SRC_CORE_COMMON_EXCEPTIONS_REPOSITORY_EXCEPTION_HPP
+
+#include <stdexcept>
+
+namespace ugame::core
+{
+
+// \brief exception class that is used by Repositories in the case that something didn't go as expected
+//
+// This exception can be used for a multitude of reasons (i.e. File opening failed, internal Repository actions failed, ...)
+//
+// \author Felix Hommel
+// \date 10/10/2025
+class RepositroyException : public std::runtime_error
+{
+public:
+    using std::runtime_error::runtime_error;
+
+    ~RepositroyException() override = default;
+
+    RepositroyException(const RepositroyException&) = default;
+    RepositroyException(RepositroyException&&) = default;
+    RepositroyException& operator=(const RepositroyException&) = default;
+    RepositroyException& operator=(RepositroyException&&) = default;
+
+private:
+};
+
+} //!ugame::core
+
+#endif //!SRC_CORE_COMMON_EXCEPTIONS_REPOSITORY_EXCEPTION_HPP
