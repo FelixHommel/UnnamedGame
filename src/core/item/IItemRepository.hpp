9a2a3860cbab726818c7e58a8ecee1c966e4ed95
Felix Hommel <felixhommel04@gmail.com>
2025-10-10 19:36:26 +0200

    refactor(items): change return type of find functions to more appropriate types

--- src/core/item/IItemRepository.hpp
+++ src/core/item/IItemRepository.hpp
@@ -4,14 +4,14 @@
 #include "common/Definitions.hpp"
 #include "Item.hpp"
 
-#include <memory>
-#include <optional>
 #include <vector>
 
 namespace ugame::core
 {
 
 /// \brief Interface to access the avilable items
+///
+/// ItemRepositories are responsible for providing callers with information about items they are requesting.
 ///
 /// \author Felix Hommel
 /// \date 10/5/2025
@@ -26,10 +26,16 @@ public:
     IItemRepository& operator=(const IItemRepository&) = default;
     IItemRepository& operator=(IItemRepository&&) = default;
 
-    /// \brief retrieve the item specified by it's ID
-    virtual std::optional<std::shared_ptr<Item>> findById(ItemID id) noexcept = 0;
+    /// \brief retrieve a item specified by it's ID
+    ///
+    /// \param id \ref ItemID of the requested Item
+    ///
+    /// \return pointer to const Item, nullptr if no Item matches \p id
+    virtual const Item* findById(ItemID id) = 0;
     /// \brief retrieve all items
-    virtual std::vector<std::shared_ptr<Item>> findAll() noexcept = 0;
+    ///
+    /// \return all avaialble items in a \ref std::vector of pointers to const Item
+    virtual std::vector<const Item*> findAll() = 0;
 };
 
 } // !ugame::core
