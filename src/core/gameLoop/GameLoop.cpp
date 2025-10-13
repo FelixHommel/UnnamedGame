3bcaea9b4909a245717e8eb8c473ccf51ff0f35e
Felix Hommel <felixhommel04@gmail.com>
2025-10-13 16:28:47 +0200

    feat(gameLoop): add simple game loop for temporary usage and verifying functionality. For that purpose also add a event for new player input

--- src/core/gameLoop/GameLoop.cpp
+++ src/core/gameLoop/GameLoop.cpp
@@ -1,6 +1,9 @@
 #include "GameLoop.hpp"
 
 #include "common/ILogger.hpp"
+#include "common/IPlayerInput.hpp"
+#include "gameLoop/EventBus.hpp"
+#include "gameLoop/events/PlayerInputReceivedEvent.hpp"
 
 #include <memory>
 #include <utility>
@@ -8,15 +11,35 @@
 namespace ugame::core
 {
 
-GameLoop::GameLoop(std::unique_ptr<ILogger> logger)
+GameLoop::GameLoop(std::unique_ptr<ILogger> logger, EventBus& bus, std::unique_ptr<IPlayerInput> input)
     : m_logger(std::move(logger))
+    , m_input(std::move(input))
 {
+    bus.subscribe<events::PlayerInputReceivedEvent>([this](const events::PlayerInputReceivedEvent& event){ handlePlayerInput(event); });
 }
 
 void GameLoop::run()
 {
     m_logger->info("game started");
+
+    while(!m_shouldQuit)
+    {
+        m_input->poll();
+    }
+
     m_logger->info("game ended");
+}
+
+/// \brief Callback function that is handling the \ref PlayerInputReceivedEvent
+///
+/// \param event input event that is being processed
+void GameLoop::handlePlayerInput(const events::PlayerInputReceivedEvent& event)
+{
+    // TODO: needs actual input handling, for now echoing the input is enough
+    m_logger->info("received input: {}", event.input);
+
+    if(event.input == "q")
+        m_shouldQuit = true;
 }
 
 } // !ugame::core
