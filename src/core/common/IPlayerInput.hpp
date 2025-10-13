0b3a541e7112235388d5c0ac1f8afcb7dc22b0b8
Felix Hommel <felixhommel04@gmail.com>
2025-10-13 16:26:57 +0200

    feat(input): add interface to get user input

---
+++ src/core/common/IPlayerInput.hpp
@@ -0,0 +1,36 @@
+#ifndef SRC_CORE_COMMON_I_PLAYER_INPUT_HPP
+#define SRC_CORE_COMMON_I_PLAYER_INPUT_HPP
+
+#include "gameLoop/EventBus.hpp"
+
+namespace ugame::core
+{
+
+/// \brief Interface that handles input from users
+///
+/// \author Felix Hommel
+/// \date 10/13/2025
+class IPlayerInput
+{
+public:
+    explicit IPlayerInput(EventBus& publisher)
+        : m_publisher(publisher)
+    {}
+
+    virtual ~IPlayerInput() = default;
+
+    IPlayerInput(const IPlayerInput&) = default;
+    IPlayerInput(IPlayerInput&&) = delete;
+    IPlayerInput& operator=(const IPlayerInput&) = delete;
+    IPlayerInput& operator=(IPlayerInput&&) = delete;
+
+    /// \brief Poll for new input
+    virtual void poll() = 0;
+
+protected:
+    EventBus& m_publisher;
+};
+
+} //!ugame::core
+
+#endif //!SRC_CORE_COMMON_I_PLAYER_INPUT_HPP
